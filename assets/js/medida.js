1317baa77b44235890a1a88031c95d418e525be3
alu0100816761 <alu0100816761@ull.edu.es>
2016-03-09 18:51:37 +0000

    Separado en ficheros y añadido el match

---
+++ assets/js/medida.js
@@ -0,0 +1,42 @@
+function Medida (valor,tipo)
+{
+    this.valor = valor;
+    this.tipo = tipo || "";
+    this.measures = {};
+
+    this.match = new function (valor) {
+      var regexp = XRegExp('^([ ]*) \n' +
+                        '(?<val> [-+]?[0-9]+(\.[0-9]+)?(?:e[+-]?[0-9]+)?) # val \n' +
+                        '([ ]*) \n' +
+                        '(?<tip> [fckFCK]) # tip \n' +
+                        '([ ]*) \n' +
+                        '(?<to> (to))? # to \n' +
+                        '([ ]*) \n' +
+                        '(?<para> [fckFCK]) # para \n' +
+                        '([ ]*)$','x');
+      valor = XRegExp.exec(valor, regexp);
+      return valor;
+    }
+}
+
+Medida.convertir = function(valor) {
+  var measures = Medida.measures;
+
+  var match = Medida.match(valor);
+  if (match) {
+    var numero = match.numero,
+        tipo   = match.tipo,
+        destino = match.destino;
+
+    try {
+      var source = new measures[tipo](numero);                  // new Fahrenheit(32)
+      var target = "to"+measures[destino].name;                 // "toCelsius"
+      return source[target]().toFixed(2) + " "+target;          // "0 Celsius"
+    }
+    catch(err) {
+      return 'Desconozco como convertir desde "'+tipo+'" hasta "'+destino+'"';
+    }
+  }
+  else
+    return "Introduzca una temperatura valida: 330e-1 F to C";
+};
